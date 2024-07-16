273f49bd25d439ff450c8e1c98e9c4dd06d0f025
Kruz Merrill <kruz.merrill04@gmail.com>
2024-07-15 20:20:47 -0600

    Guess The Number Game
    Guess a randomized number, you only have 10 tries! First project  diving into DOM in JavaScript.

---
+++ Guess The Number/script.js
@@ -0,0 +1,59 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+let score = 10;
+let highscore = 0;
+
+const displayTextCont = function (type, message) {
+  document.querySelector(type).textContent = message;
+};
+const displayStyle = function (type, variable, message) {
+  document.querySelector(type).style[variable] = message;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // When no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No number! ';
+    displayTextCont('.message', '⛔ No number! ');
+  }
+  //   When guess is correct == Wins
+  else if (guess === secretNumber) {
+    displayTextCont('.number', secretNumber);
+    displayTextCont('.message', '🎉 Correct Number!');
+    displayStyle('body', 'backgroundColor', '#60b347');
+    displayStyle('.number', 'width', '30rem');
+
+    if (score > highscore) {
+      highscore = score;
+      displayTextCont('.highscore', highscore);
+    }
+  }
+  // When Guess is Wrong!
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayTextCont(
+        '.message',
+        guess > secretNumber ? '👆 Too High!' : '👇 Too Low!'
+      );
+      score--;
+      displayTextCont('.score', score);
+    } else {
+      displayTextCont('.message', 'You lost the game!');
+      displayTextCont('.score', 0);
+      displayTextCont('.number', secretNumber);
+      displayStyle('body', 'backgroundColor', '#FF0000');
+    }
+  }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 10;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayTextCont('.message', 'Start guessing...');
+    displayStyle('body', 'backgroundColor', '#222');
+    displayStyle('.number', 'width', '15rem');
+    displayTextCont('.score', score);
+    displayTextCont('.number', '?');
+    document.querySelector('.guess').value = '';
+  });
+});
